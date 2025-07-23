00ab0d983c8ae535768c6f1f837eb9c796328e82
Isaac Newton <hanifmaulana1997@gmail.com>
2025-07-23 13:53:28 +0700

    firs

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+  return (
+    <div className='flex min-h-svh w-full items-center justify-center p-6 md:p-10 ' >
+      <div className='w-4/5 max-w-1/2 h-2/4'>
+      </div>
+    </div>
+  )
+}
+
+export default App
